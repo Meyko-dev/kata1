b9e71bd4daba86931ef735c497524fdfd888d7a7
Meyko_Dev <raphaelle.noel@gmail.com>
2025-07-04 13:33:34 +0200

    js

---
+++ script.js
@@ -0,0 +1,5 @@
+const form = document.getElementById('citationsForm');
+const citationsList = document.getElementById('citationsList');
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault(); })
