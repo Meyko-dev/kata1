56e15e7f437d33dcfe3598d4a5d3303f7cd7f1ea
Meyko_Dev <raphaelle.noel@gmail.com>
2025-07-04 13:47:09 +0200

    étape 5

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-const form = document.getElementById('citationsForm');
-const citationsList = document.getElementById('citationsList');
+const form = document.getElementById("citationsForm");
+const citationsList = document.getElementById("citationsList");
 
-form.addEventListener('submit', function(event) {
+form.addEventListener("submit", function(event) {
     event.preventDefault(); })
+
+    const text = document.getElementById("text").value;
+    const author = document.getElementById("author").value
+
+      console.log('Texte:', text);
+      console.log('Auteur:', author)
+      console.log('Données du formulaire:', {
+        text: text,
+        author: author
+    })
+
+    function addQuote(quote, author){
+        
+    }
